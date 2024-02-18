f0d09a3650b70a23a956d2e8cb075d7803ecc040
vamshu1848 <155237735+vamshu1848@users.noreply.github.com>
2024-02-18 18:02:29 +0530

    Update tomcatdeploy.groovy

--- vars/tomcatdeploy.groovy
+++ vars/tomcatdeploy.groovy
@@ -1,6 +1,6 @@
 def call (credID, warname, username, IP) {
-  sshagent(['credID']) {
-                    sh "scp -o StrictHostKeyChecking=no target/${warname} ${username}@${IP}:/opt/tomcat9/webapps"
+  sshagent([credID]) {
+                    sh "scp -o StrictHostKeyChecking=no target/${warname} ${username}@${IP}:/opt/tomcat9/webapps/"
                     sh "ssh ${username}@${IP} /opt/tomcat9/bin/shutdown.sh"
                     sh "ssh ${username}@${IP} /opt/tomcat9/bin/startup.sh"
   }
